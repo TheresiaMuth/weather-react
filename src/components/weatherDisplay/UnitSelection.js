811e87d1afb8b965900f698fb73acfb0e6dc430d
TheresiaMuth <86967085+TheresiaMuth@users.noreply.github.com>
2021-10-01 11:39:00 +0200

    Styling Unitselection

--- src/components/weatherDisplay/UnitSelection.js
+++ src/components/weatherDisplay/UnitSelection.js
@@ -1,22 +1,30 @@
 import React from "react";
 
+import "./UnitSelection.css";
+
 export default function UnitSelction(props) {
-  if (props.unit) {
+  if (true) {
     return (
-      <div className="col">
-        <span>°C|</span>
-        <a href="/" onClick={props.showFahrenheit}>
-          °F
-        </a>
-      </div>
-    );
-  } else {
-    return (
-      <div className="col">
-        <a href="/" onClick={props.showCelcius}>
+      <div className="col unitSelection">
+        <button
+          type="button"
+          id="unit-button"
+          className="btn btn-outline-light"
+          onClick={props.showCelcius}
+          disabled={props.unit}
+        >
           °C
-        </a>
-        <span>|°F</span>
+        </button>
+        <span> | </span>
+        <button
+          type="button"
+          id="unit-button"
+          className="btn btn-outline-light"
+          onClick={props.showFahrenheit}
+          disabled={!props.unit}
+        >
+          °F
+        </button>
       </div>
     );
   }
